00a7bf0975ed0c1bc9333013a14aa3db3f20bc1a
Jared Hobson <jaredmhobson@gmail.com>
2024-04-10 13:47:11 -0700

    Complete user story 25 test link to show page for admin/merchants/index_spec

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -3,11 +3,6 @@ require "rails_helper"
 RSpec.describe "the admin merchants index page" do
   before(:each) do
     @merchant_list = FactoryBot.create_list(:merchant, 3)
-    @first_merchant = @merchant_list[0]
-    @second_merchant = @merchant_list[1]
-    @third_merchant = @merchant_list[2]
-
-    @item_list = FactoryBot.create_list(:item, 10, merchant: @first_merchant)
   end
 
   describe 'User Story 24' do
@@ -17,6 +12,18 @@ RSpec.describe "the admin merchants index page" do
       within '#admin_merchant_names' do
         @merchant_list.each do |merchant|
           expect(page).to have_content(merchant.name)
+        end
+      end
+    end
+  end
+
+  describe 'User Story 25' do
+    it 'links to each merchants show page' do
+      visit admin_merchants_path
+
+      within '#admin_merchant_names' do
+        @merchant_list.each do |merchant|
+          expect(page).to have_link(merchant.name, href: admin_merchant_path(merchant))
         end
       end
     end
