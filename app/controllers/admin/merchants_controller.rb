90406ad65ab7cd104e365848f868296f8a5f7d71
Jared Hobson <jaredmhobson@gmail.com>
2024-04-11 16:36:37 -0700

    Complete US27 features for admin/merchants/index

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -14,14 +14,19 @@ class Admin::MerchantsController < ApplicationController
   def update
     merchant = Merchant.find(params[:id])
 
-    if merchant.update!(merchant_params)
-      flash[:notice] = "#{merchant.name} info updated successfully."
-      redirect_to admin_merchant_path(merchant)
+    if params[:name].present?
+      if merchant.update!(merchant_params)
+        flash[:notice] = "#{merchant.name} info updated successfully."
+        redirect_to admin_merchant_path(merchant)
+      end
+    else
+      merchant.update!(status: params[:status])
+      redirect_to admin_merchants_path
     end
   end
 
   private
   def merchant_params
-    params.permit(:name)
+    params.permit(:name, :status)
   end
 end
