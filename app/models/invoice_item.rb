ec5e4311e53cef180ae7616ba25f2069c7cce8eb
Jared Hobson <155705877+JaredMHobson@users.noreply.github.com>
2024-04-11 15:12:45 -0700

    Remove commented out text in InvoiceItem

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -4,18 +4,8 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
   has_one :merchant, through: :item
   belongs_to :invoice
-  # .transaction is a built in AR method and cant use as an association
-  # has_one :transaction, through: :invoice
   has_one :customer, through: :invoice
 
   validates :quantity, presence: true
   validates :unit_price, presence: true
 end
-
-
-# has_one **{ 
-#           :transaxtions,
-#           fk: :transaction_id,
-#           pk: :id,
-#           class_name: :Transaction
-#         }
