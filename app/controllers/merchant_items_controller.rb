d3261cc089f5d992d9b302e1d2e1ff0659437f13
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-04-15 17:45:23 -0400

    Create test for story 8, create route for edit and update merchants/items, create views for merchant item show and edit, crearte merchant_items controller actions for show edit and update

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -2,4 +2,34 @@ class MerchantItemsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
-end
+
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = @merchant.items.find(params[:id])
+  end
+
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = @merchant.items.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = @merchant.items.find(params[:id])
+
+    if params[:name].present?
+      if @item.update!(item_params)
+        flash[:notice] = "#{@item.name} info updated successfully."
+        redirect_to merchant_item_path(@merchant, @item)
+      end
+    else
+      @item.update!(item_params)
+      redirect_to edit_merchant_item_path(@merchant, @item)
+    end
+  end
+
+  private
+    def item_params
+      params.permit(:name, :description, :unit_price, :status)
+    end
+  end
